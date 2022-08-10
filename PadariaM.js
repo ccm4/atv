49d1d4b837e27715ff617314da5cf956f91475d9
Camille Cibelle <91040275+ccm4@users.noreply.github.com>
2022-08-10 09:34:39 -0300

    Add files via upload

---
+++ PadariaM.js
@@ -0,0 +1,29 @@
+funcionario1 = {nome: "ANDREI MOISÉS MEDEIROS DELFINO", matricula:"20201101110019", senha:"019", idade: 18}
+funcionario2 = {nome: "ARTHUR ARMANI ARAÚJO DA SILVA", matricula:"20201101110045", senha:"045", idade: 18}
+funcionario3 = {nome: "BEATRIZ DE BRITO DANTAS", matricula:"20201101110010", senha:"010", idade: 25}
+funcionario4 = {nome: "CAMILLE CIBELLE MEDEIROS", matricula:"20201101110023", senha:"023", idade: 32}
+funcionario5 = {nome: "CARLOS SÉRGIO BATALHA FILHO", matricula:"20201101110057", senha:"057", idade: 30}
+funcionario6 = {nome: "CECÍLIA SOUSA DE MEDEIROS", matricula:"20201101110024", senha:"024", idade: 20}
+funcionario7 = {nome: "ELLEN KARINNE ARAÚJO SILVA", matricula:"20201101110066", senha:"066", idade: 28}
+funcionario8 = {nome: "ERIC RUDNICK DE MEDEIROS", matricula:"20201101110072", senha:"072", idade: 41}
+funcionario9 = {nome: "ESTERFANY EDUARDA SILVA DE ARAÚJO", matricula:"20201101110050", senha:"050", idade: 36}
+funcionario10 = {nome: "FABRÍZIO JÔNATAS MEDEIROS SILVA E SILVA", matricula:"20201101110075", senha:"075", idade: 48}
+funcionario11 = {nome: "GABRIEL VICTOR DE SOUZA FERNANDES", matricula:"20201101110068", senha:"068", idade: 39}
+funcionario12 = {nome: "GLAUCYRLEI PEREIRA DA SILVA", matricula:"20201101110036", senha:"036", idade: 26}
+funcionario13 = {nome: "GUSTAVO CESAR DE ARAUJO DANTAS", matricula:"20201101110065", senha:"065", idade: 44}
+funcionario14 = {nome: "ISADORA BEATRIZ LUCENA DE MEDEIROS", matricula:"20201101110051", senha:"051", idade: 41}
+funcionario15 = {nome: "LUCAS GABRIEL PEREIRA COSTA", matricula:"20201101110005", senha:"005", idade: 37}
+funcionario16 = {nome: "MAINARA FABRÍCIA DANTAS SILVA", matricula:"20201101110006", senha:"006", idade: 43}
+funcionario17 = {nome: "MANOEL MURILO DE MEDEIROS FILHO", matricula:"20201101110003", senha:"003", idade: 23}
+funcionario18 = {nome: "MARIA EDUARDA DE ARAÚJO RIBEIRO", matricula:"20201101110061", senha:"061", idade: 45}
+funcionario19 = {nome: "RAYSSA KAREM FERNANDES DANTAS", matricula:"20201101110018", senha:"018", idade: 19}
+funcionario20 = {nome: "MARIA LUIZA BORGES RODRIGUES", matricula:"20201101110063", senha:"063", idade: 32}
+
+
+functionarios = [funcionario1, funcionario2, funcionario3, funcionario4, funcionario5, funcionario6, funcionario7, funcionario8, funcionario9, funcionario10, funcionario11, funcionario12, funcionario13, funcionario14, funcionario15, funcionario16, funcionario17, funcionario18, funcionario19, funcionario20]
+
+produto1 = {nome: "biscoito morando", marca:"nestle", id: "001", preco:10.40}
+produto2 = {nome: "pao morfado", marca:"wolkasgen", id: "002", preco:1.0}
+
+produtos = [produto1, produto2]
+
